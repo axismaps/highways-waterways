34a97063e364ce70c5ff3b0a75bc329a385eb85c
Benjamin Wellington <benhwell@gmail.com>
2019-06-28 11:45:43 -0400

    Add app state properties

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import Sidebar from '../sidebar/Sidebar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-    </div>
-  );
+class App extends React.Component() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      year: 1950,
+      sidebarOpen: true,
+      rasterProbe: null,
+      overlay: null,
+      availableLayers: [],
+      currentLayers: [],
+      currentFilters: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+      </div>
+    );
+  }
 }
 
 export default App;
