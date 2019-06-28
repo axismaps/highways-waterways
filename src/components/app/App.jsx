34119d2acc62b01531a359d832a6317e41dc6e35
Benjamin Wellington <benhwell@gmail.com>
2019-06-28 11:49:46 -0400

    Fix typo

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Sidebar from '../sidebar/Sidebar';
 import './App.css';
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor(props) {
     super(props);
 
