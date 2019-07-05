d4fbfbcc18c18e683bb50683e1a7b030c936e34c
Benjamin Wellington <benhwell@gmail.com>
2019-07-05 14:31:07 -0400

    Format slider container

--- src/components/header/HeaderTimeline.jsx
+++ src/components/header/HeaderTimeline.jsx
@@ -13,10 +13,47 @@ import PropTypes from 'prop-types';
  */
 
 class HeaderTimeline extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.timelineRef = React.createRef();
+    this.state = {
+      containerWidth: null,
+    };
+  }
+
+  componentDidMount() {
+    const timelineNode = this.timelineRef.current;
+    console.log('timelinenode', timelineNode);
+    if (timelineNode !== undefined) {
+      this.setState({
+        containerWidth: timelineNode.getBoundingClientRect().width,
+      });
+    }
+  }
+
+  getTimeline() {
+    const { containerWidth } = this.state;
+    if (containerWidth === null) return null;
+    const padding = {
+      left: 20,
+      right: 20,
+      top: 20,
+      bottom: 20,
+    };
+    const height = 20;
+    return (
+      <div className="timeline__slider">
+        slider
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div className="header__timeline">
-        Timeline
+      <div className="header__timeline" ref={this.timelineRef}>
+        <div className="timeline__slider">
+          timeline slider
+        </div>
       </div>
     );
   }
