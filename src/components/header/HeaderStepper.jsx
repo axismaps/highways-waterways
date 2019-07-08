8bae7df6a2c7618ada9637cabc7b63653f815301
Benjamin Wellington <benhwell@gmail.com>
2019-07-08 16:47:08 -0400

    Add unstyled year stepper, install fontawesome packages

--- src/components/header/HeaderStepper.jsx
+++ src/components/header/HeaderStepper.jsx
@@ -13,9 +13,34 @@ import PropTypes from 'prop-types';
 
 class HeaderStepper extends React.PureComponent {
   render() {
+    const {
+      year,
+      setYear,
+    } = this.props;
+    const roundYear = Math.round(year);
+    const stepBack = () => {
+      setYear(roundYear - 1);
+    };
+    const stepForward = () => {
+      setYear(roundYear + 1);
+    }
     return (
       <div className="header__stepper">
-        Stepper
+        <div
+          className="header__stepper-left header__stepper-button"
+          onClick={stepBack}
+        >
+          prev
+        </div>
+        <div className="header__stepper-year">
+          {roundYear}
+        </div>
+        <div
+          className="header__stepper-next header__stepper-button"
+          onClick={stepForward}
+        >
+          next
+        </div>
       </div>
     );
   }
