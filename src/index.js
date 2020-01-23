930e4e6540900218f559720df45dd10c2c343602
David Heyman <dave@axismaps.com>
2020-01-23 08:09:28 +0000

    Building with core-js instead of babel polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@babel/polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import './index.scss';
 import App from './components/app/App';
 
