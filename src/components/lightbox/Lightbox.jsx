cf7f5660ec9463f29045deeadb370abf545c3460
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-05-07 17:10:12 -0300

    fix: Image Lightbox: missing “View on Artstor button”, author and credit line

--- src/components/lightbox/Lightbox.jsx
+++ src/components/lightbox/Lightbox.jsx
@@ -5,12 +5,36 @@ import './Lightbox.scss';
 class Lightbox extends React.PureComponent {
   render() {
     const { clearLightbox, lightboxRaster } = this.props;
+    console.log(lightboxRaster);
     return (
       <div className="lightbox" onClick={clearLightbox}>
         <div className="lightbox__container">
           <div className="lightbox__inner">
             <img className="lightbox__img" src={lightboxRaster.raster.thumb} />
-            <p>lightbox: {lightboxRaster.raster.title}</p>
+            <div className="lightbox__footer">
+              <div>
+                <span>{lightboxRaster.raster.creator} Malta, Algusto</span>
+                <br />
+                <span className="lightbox__footer-imgName">
+                  {lightboxRaster.raster.title}
+                </span>
+                <br />
+                <span className="lightbox__footer-imgCredit">
+                  1912 [Instituto Moreira Salles]
+                  {lightboxRaster.raster.credit}
+                </span>
+                <br />
+              </div>
+              <div>
+                <a
+                  className="lightbox__footer-button"
+                  href={lightboxRaster.raster.thumb}
+                  target="blank"
+                >
+                  View image on Artstor
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -23,8 +47,8 @@ Lightbox.propTypes = {
   clearLightbox: PropTypes.func.isRequired,
   lightboxRaster: PropTypes.shape({
     type: PropTypes.string,
-    raster: PropTypes.object
-  }).isRequired
+    raster: PropTypes.object,
+  }).isRequired,
 };
 
 export default Lightbox;
